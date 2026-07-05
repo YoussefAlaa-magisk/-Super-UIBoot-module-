b3dda3d736ef86edf04dacaf71b829664ab26c7b
YoussefAlaa-magisk <yasserahmed55525@gmail.com>
2026-07-05 03:24:19 +0300

    Add additional getprop commands for system info

--- action.sh
+++ action.sh
@@ -2,4 +2,7 @@ MODDIR="$(find /data/adb -type d -name samsung_SuperUI_boot)"
 
 echo "SuperUI boot is here for faster boot!!"
 getprop ro.product.model
+getprop ro.product.system.brand
+getprop ro.product.system.device
+
 
