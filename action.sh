3a176122b7403510da05382864b10f526f37b26d
YoussefAlaa-magisk <yasserahmed55525@gmail.com>
2026-07-08 21:52:44 +0300

    Update MODDIR path and enhance output messages

--- action.sh
+++ action.sh
@@ -1,8 +1,39 @@
-MODDIR="$(find /data/adb -type d -name samsung_SuperUI_boot)"
+MODDIR="$(find /data/adb/modules -type d -name samsung_SuperUI_boot)"
 
-echo "SuperUI boot is here for faster boot!!"
+echo "=============================="
+echo "SuperUI boot module is here for faster boot!!"
+echo "hello from youssef alaa module"
+echo "=============================="
+
+echo "Getting Super UI Version..."
+sleep 2
+
+echo "Super UI Boot Version 1.1!!"
+
+echo "=============================="
+echo "Device Information"
+echo "=============================="
+
+echo "Model:"
 getprop ro.product.model
+
+echo "Brand:"
 getprop ro.product.system.brand
+
+echo "Device:"
 getprop ro.product.system.device
 
+echo "Vendor Model:"
+getprop ro.product.vendor.model
 
+echo "ODM:"
+getprop ro.product.odm.name
+
+echo "Board:"
+getprop ro.product.board
+
+echo "Vendor:"
+getprop ro.product.vendor.name
+
+echo "Vendor Device:"
+getprop ro.product.vendor.device
