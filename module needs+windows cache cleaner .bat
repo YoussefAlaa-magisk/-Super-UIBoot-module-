c086695cbeae57ece0ce69f2b508fa41db77ee72
YoussefAlaa-magisk <yasserahmed55525@gmail.com>
2026-09-22 23:30:57 +0300

    Add module needs for Windows cache cleaner

---
+++ module needs+windows cache cleaner .bat
@@ -0,0 +1,28 @@
+@echo off
+timeout 5
+echo ✅️ = needed 
+echo ❌️ = unsupported/not recommended nr= echo not recommended unsupported = uns
+echo ⚠️ = 50 50 
+
+
+echo - oneui✅️
+echo - samsung✅️
+echo - 2400×1080✅️
+echo - 2300×1080 ⚠️ maybe 
+echo - rooted device && unlocked bootloader✅️
+echo - touchwiz❌️ /uns
+echo - 1280×720❌️ /uns
+echo - not samsung❌️ /uns
+echo - flagship device❌️ /nr
+
+del /q /f /s "%TEMP%\*" >nul 2>&1
+for /d %%D in ("%TEMP%\*") do rd /s /q "%%D" >nul 2>&1
+
+
+del /q /f /s "C:\Windows\Temp\*" >nul 2>&1
+for /d %%D in ("C:\Windows\Temp\*") do rd /s /q "%%D" >nul 2>&1
+
+ipconfig /flushdns >nul
+
+timeout 60
+exit
